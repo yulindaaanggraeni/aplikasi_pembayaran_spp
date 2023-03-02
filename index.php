64c79840bdfd7ae9b88fba6ce3d8e8c4868bed89
yulindaaanggraeni <126765603+yulindaaanggraeni@users.noreply.github.com>
2023-03-02 16:14:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login Siswa</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body  style="background-image: url(rina.jpg) background-repeat:no-repeat; " >
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Login </h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+		      	<div class="icon d-flex align-items-center justify-content-center">
+		      		<span class="fa fa-user-o"></span>
+		      	</div>
+		      	<h3 class="text-center mb-4">Siswa</h3>
+						<form action="proses-login-siswa.php" class="login-form" method="post">
+		      		<div class="form-group">
+		      			<input type="number" name="nis" class="form-control" placeholder="Masukan NISN anda...." required>
+		      		</div>
+	            <div class="form-group d-flex">
+	             <input type="number" name="nisn" class="form-control" placeholder="Masukan NIS anda...." required>
+	            </div>
+	           
+	            <div class="form-group">
+	            	<button type="submit" class="btn btn-primary">LOGIN</button><br>
+	            	<a href="index2.php">Login Ke petugas </a>
+	            </div>
+	          </form>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
